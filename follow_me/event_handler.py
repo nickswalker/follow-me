fa3406e6f001266d394dd37d179e27308a2ad85c
Nick Walker <nick@nickwalker.us>
2017-01-23 00:13:05 -0600

    Add basic periodic commit and push Add GitPython dependency Add session wrapup commit squashing Add basic error messages, CLI

--- follow_me/event_handler.py
+++ follow_me/event_handler.py
@@ -1,7 +1,18 @@
+from threading import Timer
+import os
 from watchdog.events import PatternMatchingEventHandler
 
 
-class EventHandler(PatternMatchingEventHandler):
+class GitCommittingEventHandler(PatternMatchingEventHandler):
+
+    def __init__(self, repo, remote):
+        super().__init__()
+        self.repo = repo
+        self.remote = remote
+        self.modifications_timer = None
+        self.modifications = []
+        self.baseline_timer = Timer(60.0, self._baseline_expired)
+        self.commit_hashes = []
 
     def process(self, event):
         """
@@ -12,6 +23,12 @@ class EventHandler(PatternMatchingEventHandler):
         event.src_path
             path/to/observed/file
         """
+        if self.modifications_timer:
+            self.modifications_timer.cancel()
+        self.modifications_timer = Timer(20.0, self._modifications_timer_expired)
+        self.modifications_timer.start()
+
+        self.modifications.append(os.path.abspath(event.src_path))
         print(event.src_path, event.event_type)
 
     def on_modified(self, event):
@@ -19,3 +36,39 @@ class EventHandler(PatternMatchingEventHandler):
 
     def on_created(self, event):
         self.process(event)
+
+    def on_deleted(self, event):
+        self.process(event)
+
+    def on_moved(self, event):
+        self.process(event)
+
+    def _filter_modification(self, modification):
+        return os.path.exists(modification) and not ".git" in modification and modification != self.repo.working_dir
+
+    def commitandpush(self):
+        to_commit = [mod for mod in self.modifications if self._filter_modification(mod)]
+        self.modifications = []
+        if len(to_commit) == 0:
+            return
+        self.repo.index.add(to_commit)
+        commit = self.repo.index.commit("Automated commit")
+        self.remote.push()
+        self.commit_hashes.append(commit)
+        print("Pushed " + str(commit)[:10])
+
+    def _modifications_timer_expired(self):
+        self.baseline_timer.cancel()
+
+        self.commitandpush()
+        self.modifications_timer = None
+        self.baseline_timer = Timer(60.0, self._baseline_expired)
+        self.baseline_timer.start()
+
+    def _baseline_expired(self):
+        if self.modifications_timer:
+            self.modifications_timer.cancel()
+            self.modifications_timer = None
+        self.commitandpush()
+        self.baseline_timer = Timer(60.0, self._baseline_expired)
+        self.baseline_timer.start()
--- run.py
+++ run.py
@@ -1,15 +1,58 @@
 import time
 import sys
+import os
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
-from follow_me.event_handler import EventHandler
+from git import Repo, InvalidGitRepositoryError
+
+from follow_me.event_handler import GitCommittingEventHandler
 
 
 def main():
     args = sys.argv[1:]
+    path = args[0]
+    abs_path = os.path.abspath(path)
+    if not os.path.isdir(path):
+        sys.stderr.write(abs_path + " is not a valid path.")
+        exit(1)
+    try:
+        repo = Repo(path)
+    except InvalidGitRepositoryError as e:
+        sys.stderr.write("Please create a Git repository in " + abs_path)
+        exit(1)
+
+    if len(repo.branches) == 0:
+        sys.stderr.write("Repo has no branches. You may need to add an initial commit if this is a new repo.")
+        exit(1)
+
+    print("Attached to repo on branch " + repo.active_branch.name)
+
+    if len(repo.remotes) == 0:
+        sys.stderr.write("Please configure an origin remote to sync with.")
+        exit(1)
+
+    origin = None
+    for remote in repo.remotes:
+        if remote.name == "origin":
+            origin = remote
+
+    if origin is None:
+        sys.stderr.write("Did not find an on origin remote. Please configure origin.")
+        exit(1)
+
+    try:
+        pass
+        # branch_has_remote = repo.active_branch.remote_name
+    except ValueError:
+        sys.stderr.write("Current branch doesn't have a remote. Please set its upstream.")
+        exit(1)
+
+    origin.pull()
+    print("Local and remote are in sync. Ready to go.")
+    handler = GitCommittingEventHandler(repo, origin)
     observer = Observer()
-    observer.schedule(EventHandler(), path=args[0] if args else '.')
+    observer.schedule(handler, path=path)
     observer.start()
 
     try:
@@ -19,6 +62,19 @@ def main():
         observer.stop()
 
     observer.join()
+
+    commits = handler.commit_hashes
+    if len(commits) == 0:
+        exit(0)
+
+    first_commit = commits[0]
+    repo.git.reset("--soft", str(first_commit) + "^")
+
+    print("Compressed " + str(len(commits)) + " automated commits.")
+    commit_message = input("Enter a commit message for this session:")
+    repo.git.commit(m=commit_message)
+
+    repo.git.push(force=True)
 
 if __name__ == '__main__':
     main()
